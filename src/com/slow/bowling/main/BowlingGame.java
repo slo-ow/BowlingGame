46bedf0a1d2885b5a60d4fdf38ece7d717122243
slo_ow <syntaxflow93@gmail.com>
2022-09-17 13:41:35 +0900

    update main & add startGame()

--- src/com/slow/bowling/main/BowlingGame.java
+++ src/com/slow/bowling/main/BowlingGame.java
@@ -1,13 +1,52 @@
 package com.slow.bowling.main;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 public class BowlingGame {
-    int pins;
+    int[] rolls; //10프레임 게임동안 점수가 저장될 배열 선언
 
-    public BowlingGame() {
-        this.pins = 20;
+    int currentRoll; //프레임 값 증가
+
+    public BowlingGame() { this.rolls = new int[21];} //생성자 호출과 동시에 점수가 저장될 배열 공간 생성, 최대 21 frame
+
+    public void roll(int p) { rolls[currentRoll++] = p;} //프레임 0번쨰 부터 차례로 값을 추가함 최대 21 프레임까지
+
+    private void socore(){
+        int score = 0;
+        int frame = 0;
+
+
+    }
+    public void startGame(){
+        Scanner scn = new Scanner(System.in);
+        List<String> tkList = new ArrayList<>(); // 토큰값을 한번 확인할 때 마다 토큰을 잃어버리기 때문에 저장해둘 공간 선언
+        try {
+            System.out.print("점수 등록 : ");
+            String str = scn.nextLine();
+            StringTokenizer st = new StringTokenizer(str, ", ");
+            int count = st.countTokens();// 프레임수를 가져옴
+            System.out.println("프레임 수 : " + count);
+            while (st.hasMoreTokens()) { // 각 프레임에 담겨있는 값을 가져옴
+                tkList.add(st.nextToken()); // 오류검사를 위해 리스트에 추가
+            }
+            for (int i = 0; i < tkList.size(); i++) {
+                if (10 < Integer.parseInt(tkList.get(i))) { // 1프레임의 입력값이 10 보다 큰 값이 입력될 경우 에러 발생
+                    throw new Exception("한 프레임당 최대 점수는 10점 입니다.");
+                }
+            }
+            System.out.println("Clear"); // 여기까지 넘어오면 데이터에 문제는 없음, 값 출력해서 보여주면 됨
+            System.out.println("- 사용자 점수 출력 -");
+            for(String tk : tkList) {
+                System.out.printf(tk+"\t");
+            }
+            System.out.println();
+        } catch (Exception e) {
+            e.getStackTrace();
+            System.out.println(" 입력 오류 : " + e.getMessage());
+        }
     }
 
-    public int getPins() {
-        return pins;
-    }
 }
--- src/com/slow/bowling/main/BowlingGameMain.java
+++ src/com/slow/bowling/main/BowlingGameMain.java
@@ -1,5 +1,41 @@
 package com.slow.bowling.main;
 
-public class BowlingGameMain {
+import java.util.Scanner;
 
+public class BowlingGameMain {
+    private BowlingGame g; //Singleton 객체 선언
+    public void setUp(){ this.g = new BowlingGame(); } //Singleton 객체 생성 메소드
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        BowlingGameMain main = new BowlingGameMain();
+        main.setUp();
+        while (true) {
+            System.out.println("- Bowling Game -");
+            System.out.println("[1] 게임시작");
+            System.out.println("[2] 결과출력");
+            System.out.println("[3] 규칙설명");
+            System.out.println("[4] 게임종료");
+            System.out.print("메뉴 선택 ?");
+            int sw = scn.nextInt();
+            switch (sw) {
+                case 1:
+                    System.out.println("입력 1");
+                    main.g.startGame();
+                    break;
+                case 2:
+                    System.out.println("입력 2");
+                    break;
+                case 3:
+                    System.out.println("입력 3");
+                    //규칙 설명서 출력
+                    break;
+                case 4:
+                    System.out.println("입력 4");
+                    System.out.println("볼링 게임을 종료 합니다.");
+                    System.exit(0);
+                default:
+                    System.out.println("입력오류 / 처음부터 다시 입력하세요");
+            }
+        }
+    }
 }
